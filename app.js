b198d65f74941f3ebafb703c0739df038801a46a
Denporty <victor.cabot@edu.devinci.fr>
2022-04-26 14:03:55 +0200

    Add index.html + update app.js

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express')
-const userAPI = require('./api/user/routes');
+const userAPI = require('./api/user/routes')
+const path = require('path')
 const app = express()
 
+app.locals.title = 'myApp'
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.sendFile(path.join(__dirname, '/index.html'))
 })
 
-userAPI.createRoutes(app);
+userAPI.createRoutes(app)
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Example app listening on port 3000`)
-
 })
