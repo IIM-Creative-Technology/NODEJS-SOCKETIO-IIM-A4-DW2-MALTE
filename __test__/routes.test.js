e14ebbe01a992c578f9f8311351b1a75ea2bb642
Julien <julien.quennehen@edu.devinci.fr>
2022-04-27 15:22:45 +0200

    test: 💍 first unit test on GET request with a mock

--- __test__/routes.test.js
+++ __test__/routes.test.js
@@ -17,3 +17,32 @@ test("test if user CRUD req res.code is 200 (succes)", async () => {
   // expect(res.statusCode).toBe(200);
   return true;
 });
+
+jest.mock(() => [
+  {
+    Firstname: "NJ",
+    Lastname: "Trenton",
+    Email: "nj.trenton@gmail.com",
+    Password: "hashedpasswordTested",
+  },
+]); //callback function with mock data
+
+let firstState;
+describe("server-routes GET user", () => {
+  it("GET /user - success", async () => {
+    const { body } = await request(app).get("/user");
+    expect(body).toEqual([
+      {
+        Firstname: "NJ",
+        Lastname: "Trenton",
+        Email: "nj.trenton@gmail.com",
+        Password: "hashedpasswordTested",
+      },
+    ]);
+    firstState = body[0];
+  });
+  it("GET /user/:id - success", async () => {
+    const { body } = await request(app).get(`/user/${firstState.state}`);
+    expect(body).toEqual(firstState);
+  });
+});
