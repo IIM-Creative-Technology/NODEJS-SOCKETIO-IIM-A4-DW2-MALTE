fedfcf839b15a461c7c8eab8c6aeefd81b9914de
Brazzolotto Nicolas <nicolas.brazzolotto@edu.devinci.fr>
2022-04-27 20:01:42 +0200

    feat(config.js): change js file to json

---
+++ config/config.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+module.exports = {
+    development: {
+        username: 'postgres',
+        password: 'postgres',
+        database: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        dialect: 'postgresql'
+    },
+    production: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'postgresql',
+    }
+};
