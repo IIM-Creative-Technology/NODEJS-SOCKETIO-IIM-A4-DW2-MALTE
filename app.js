f793a28085a5fa12655433e209e031b14a74c0f2
Brazzolotto Nicolas <nicolas.brazzolotto@edu.devinci.fr>
2022-04-27 15:00:51 +0200

    feat(app): add socket

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { createServer } = require('https');
+const { createServer } = require('http');
 const sequelize = require('./data/postgresql/data/db');
 const router = require('./routes/router');
 const { readFileSync } = require('fs');
+const {Server} = require('socket.io');
 
 // SSL Options for the HTTPS
 const optionsSSl = {
@@ -12,7 +13,8 @@ const optionsSSl = {
 
 // Create server
 const app = express();
-const httpsServer = createServer(optionsSSl);
+const httpsServer = createServer(app);
+const io = new Server(httpsServer);
 
 
 if(process.env.NODE_ENV !== 'production') {
@@ -37,4 +39,12 @@ main();
 
 httpsServer.listen(process.env.PORT || 3000, () => {
     console.log(`Example app listening on port 3000`)
+});
+
+// Socket IO
+io.on("connection", (socket) => {
+    socket.on("to-server", () => {
+        console.log("Socket received from client !");
+        socket.emit("to-client");
+    });
 });
