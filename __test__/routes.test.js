11a35812be86b7b7b466931d2e45275d7a0480a7
Julien <julien.quennehen@edu.devinci.fr>
2022-04-28 11:49:42 +0200

    test: 💍 improving mocks

--- __test__/routes.test.js
+++ __test__/routes.test.js
@@ -4,11 +4,13 @@ const app = express();
 const router = require("../routes/router");
 const user = require("../routes/user");
 
+//jest all user modules
+jest.mock("../routes/user.js");
+
 test("test if get req res.code is 200 (succes)", async () => {
   app.use("/", router);
   const res = await request(app).get("/");
   expect(res.statusCode).toBe(200);
-  return true;
 });
 
 test("mock implementation", () => {
@@ -16,4 +18,35 @@ test("mock implementation", () => {
 
   expect(mock("foo")).toBe("bar");
   expect(mock).toHaveBeenCalledWith("foo");
+});
+
+test("calls user.list, GET all users", () => {
+  const mock = jest.fn().mockImplementation(() => "User");
+  app.use("/", router);
+  expect(app.list).toBe(undefined);
+});
+
+const getFirstAlbumTitle = require("./index");
+const axios = require("axios");
+
+jest.mock("app");
+
+it("returns the title of the first album", async () => {
+  app.get.mockResolvedValue({
+    data: [
+      {
+        userId: 1,
+        id: 1,
+        title: "My First Album",
+      },
+      {
+        userId: 1,
+        id: 2,
+        title: "Album: The Sequel",
+      },
+    ],
+  });
+
+  const title = await getFirstAlbumTitle();
+  expect(title).toEqual("My First Album");
 });
