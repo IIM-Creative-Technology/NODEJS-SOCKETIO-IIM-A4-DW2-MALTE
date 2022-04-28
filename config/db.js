fdeae3042dd7db704bcea4b89fecdbc3d1298e58
AntoineDrsl <antoine.durussel@gmail.com>
2022-04-28 11:06:53 +0200

    chore(db.js): fix development env

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 const { readFileSync } = require('fs');
 require('dotenv').config();
 
-const sequelize = process.env.NODE_ENV == 'local' 
+const sequelize = process.env.NODE_ENV == 'development' 
   ? new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
       host: process.env.DB_HOST,
       dialect: 'postgres'
