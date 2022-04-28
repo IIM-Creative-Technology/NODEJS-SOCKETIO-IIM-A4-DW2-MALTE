fead8849db10fed88bd552ac5288d4452a92772a
Julien <julien.quennehen@edu.devinci.fr>
2022-04-28 13:39:46 +0200

    test: 💍 adding routes testing of api-docs and trying mocks

--- __test__/routes.test.js
+++ __test__/routes.test.js
@@ -1,52 +1,36 @@
 const express = require("express");
 const request = require("supertest");
 const app = express();
+const Database = require("../config/db");
+const config = require("../config/config");
+const User = require("../models/user");
 const router = require("../routes/router");
 const user = require("../routes/user");
+const userData = require("./db.test.json");
 
-//jest all user modules
-jest.mock("../routes/user.js");
+it("test if get req /user res.code is 200 (succes)", async () => {
+  app.use("/user", router);
+  const res = await request(app).get("/user");
+  expect(res.statusCode).toEqual(200);
+});
 
-test("test if get req res.code is 200 (succes)", async () => {
-  app.use("/", router);
-  const res = await request(app).get("/");
-  expect(res.statusCode).toBe(200);
+describe("User API", () => {
+  test("test if get req res.code is 200 (succes)", async () => {
+    app.use("/", router);
+    const res = await request(app).get("/");
+    expect(res.statusCode).toEqual(200);
+  });
+});
+
+it("test if get req /user res.code is 200 (succes)", async () => {
+  app.use("/api-docs", router);
+  const res = await request(app).get("/api-docs");
+  expect(res.statusCode).toEqual(301);
 });
 
 test("mock implementation", () => {
-  const mock = jest.fn().mockImplementation(() => "bar");
+  const mock = jest.fn().mockImplementation(() => "test");
 
-  expect(mock("foo")).toBe("bar");
-  expect(mock).toHaveBeenCalledWith("foo");
-});
-
-test("calls user.list, GET all users", () => {
-  const mock = jest.fn().mockImplementation(() => "User");
-  app.use("/", router);
-  expect(app.list).toBe(undefined);
-});
-
-const getFirstAlbumTitle = require("./index");
-const axios = require("axios");
-
-jest.mock("app");
-
-it("returns the title of the first album", async () => {
-  app.get.mockResolvedValue({
-    data: [
-      {
-        userId: 1,
-        id: 1,
-        title: "My First Album",
-      },
-      {
-        userId: 1,
-        id: 2,
-        title: "Album: The Sequel",
-      },
-    ],
-  });
-
-  const title = await getFirstAlbumTitle();
-  expect(title).toEqual("My First Album");
+  expect(mock("essai")).toBe("test");
+  expect(mock).toHaveBeenCalledWith("essai");
 });
