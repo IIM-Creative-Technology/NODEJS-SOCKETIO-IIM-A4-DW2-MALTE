d7f15e9848b77f739943e10490897e24f905349e
AntoineDrsl <antoine.durussel@gmail.com>
2022-04-28 12:29:21 +0200

    feat(app): add document upload

--- controllers/document.js
+++ controllers/document.js
@@ -1,5 +1,24 @@
-const bcrypt = require('bcrypt');
 const Document = require('../models/document');
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
+
+const mimeTypes = (document, mimeTypes = []) => {
+    if(!mimeTypes.includes(document.mimetype)) {
+        return false;
+    }
+
+    return true;
+}
+
+const fileFilter = (req, file, next) => {
+    const mimeTypeOk = mimeTypes(file, ['application/pdf']);
+    if(!mimeTypeOk) {
+        next(new Error('Invalid file type. Only pdf files are allowed.'));
+    }
+
+    next(null, true);
+}
 
 class DocumentController {
 
@@ -27,21 +46,53 @@ class DocumentController {
 
     static async create(req, res) 
     {
-        // Check POST params
-        if(!req.body.name) {
-            return res.status(400).end('name is required');
-        }
-        if(!req.body.type) {
-            return res.status(400).end('Document type is required');
-        }
-        
-        // Insert document
-        const document = await Document.create({
-            name: req.body.name,
-            type: req.body.type,
+        const dir = 'uploads/';
+
+        // Document storage 
+        const storage = multer.diskStorage({
+            destination: (req, file, next) => {
+                return next(null, dir);
+            },
+            filename: async (req, file, next) => {
+                const extension = file.mimetype == 'application/pdf' ? '.pdf' : false;
+                const type = file.mimetype == 'application/pdf' ? 'pdf' : false;
+                const name = path.parse(file.originalname).name;
+
+                if(!extension) {
+                    return Error;
+                }
+
+                // Insert document
+                const document = await Document.create({
+                    name: name,
+                    type: type,
+                });
+
+                next(null, document.id + extension);
+            }
         });
 
-        return res.json(document);
+        // Filters
+        const upload = multer({
+            storage: storage,
+            limits: {
+                files: 1,
+                fileSize: 1024 * 1024
+            },
+            fileFilter: fileFilter
+        }).single('document');
+        
+        // Upload document
+        upload(req, res, async (err) => {
+            if(err || !req.file?.filename) {
+                return res.status(500).json('Document parameter is required.');
+            }
+
+            const documentId = path.parse(req.file.filename).name;
+            const document = await Document.findByPk(documentId);
+
+            return res.status(200).json(document);
+        });
     }
 
     static async update(req, res)
