2107ac3706f2288105e1ad7ff02443b59b541aec
AntoineDrsl <antoine.durussel@gmail.com>
2022-04-28 10:45:37 +0200

    feat(router.js): add documents routes in router

--- routes/router.js
+++ routes/router.js
@@ -3,9 +3,11 @@ const router = express.Router()
 const MainController = require('../controllers/main');
 
 const userAPI = require('./user');
+const documentAPI = require('./document');
 
 router.get('/', MainController.index)
 
 userAPI.createRoutes(router);
+documentAPI.createRoutes(router);
 
 module.exports = router;
